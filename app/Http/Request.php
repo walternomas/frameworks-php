7eff2921b45f112c67f9f695f84754a0c5729712
Walter Barrios <wbarrios@verifarma.com>
2022-01-31 18:52:59 -0300

    config Request

---
+++ app/Http/Request.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http;
+
+class Request
+{
+    protected $segments = [];
+    protected $controller;
+    protected $method;
+
+    public function __construct()
+    {
+        // platzi.test/servicios/index
+	$this->segments = explode('/', $_SERVER['REQUEST_URI']);
+
+	$this->setController();
+	$this->setMethod();
+    }
+
+    public function setController()
+    {
+        $this->controller = empty($this->segments[1]) ? 'home' : $this->segments[1];
+    }
+
+    public function setMethod()
+    {
+        $this->method = empty($this->segments[2]) ? 'index' : $this->segments[2];
+    }
+}

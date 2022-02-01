243b428ae5d9c91f44fbea30b43fe1a097f4bb1c
Walter Barrios <wbarrios@verifarma.com>
2022-02-01 07:55:44 -0300

    change layout.php

--- views/layout.php
+++ views/layout.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Mi sitio web</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     </head>
     <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
             <div class="container">
 		<a href="/" class="navbar-brand h1">FB</a>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Home</a>
+                    </li>
+                </ul>
             </div>
 	</nav>
 

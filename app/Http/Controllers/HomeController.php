8e34e0615e4f33ab9a4b6751e70e00616de9b496
Walter Barrios <wbarrios@verifarma.com>
2022-02-01 07:31:30 -0300

    config and use of helpers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Response; // para evitar usar la ruta completa en el return new.
+//use App\Http\Response; // para evitar usar la ruta completa en el return new. //lo cambiamos para usar el helper.
 
 class HomeController
 {
     public function index()
     {
 	    // return new \App\Http\Response('home'); // El primer backslash es para que comience desde la raiz.
-	    return new Response('home');
+	    //return new Response('home'); lo cambiamos para usar el helper.
+
+	    return view('home');
     }
 }

46b14a33e5b336c5794220cb2d3037b1aae64466
Walter Barrios <wbarrios@verifarma.com>
2022-02-01 07:13:43 -0300

    create and config views

---
+++ views/home.php
@@ -0,0 +1,4 @@
+<div class="col-8">
+    <h1>Página Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sit</p>
+</div>
---
+++ views/layout.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    </head>
+    <body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+            <div class="container">
+		<a href="/" class="navbar-brand h1">FB</a>
+            </div>
+	</nav>
+
+        <div class="container">
+	    <div class="row">
+                <?= $content;?>
+            </div>
+        </div>
+    </body>
+</html>

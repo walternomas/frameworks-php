68f938d45925e88ce5ff163856eae88511b5a9b9
Walter Barrios <wbarrios@verifarma.com>
2022-02-01 07:34:39 -0300

    clean code of HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,15 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-//use App\Http\Response; // para evitar usar la ruta completa en el return new. //lo cambiamos para usar el helper.
 
 class HomeController
 {
     public function index()
     {
-	    // return new \App\Http\Response('home'); // El primer backslash es para que comience desde la raiz.
-	    //return new Response('home'); lo cambiamos para usar el helper.
-
-	    return view('home');
+        return view('home');
     }
 }
